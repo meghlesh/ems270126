fba2c1f6e0246be21d450e0134a2dda690cd1a63
meghlesh <meghachaudhari24@gmail.com>
2026-02-09 11:31:32 +0530

    Update appInsights.js

--- CWS-Client/src/appInsights.js
+++ CWS-Client/src/appInsights.js
@@ -2,17 +2,14 @@ import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 
 const appInsights = new ApplicationInsights({
   config: {
-    connectionString:
-      "InstrumentationKey=549ddc10-4942-43aa-bfaa-244ae9fba8e2",
-    enableAutoRouteTracking: true
-  }
+    instrumentationKey: "549ddc10-4942-43aa-bfaa-244ae9fba8e2",
+    enableAutoRouteTracking: true,
+  },
 });
 
-try {
-  appInsights.loadAppInsights();
-} catch (e) {
-  console.warn("Application Insights failed to load", e);
-}
+appInsights.loadAppInsights();
+appInsights.trackPageView();
 
 export { appInsights };
+
 
