7ee965ef8c16a1c77cdc04d5f637f41a1e730e4d
meghlesh <meghachaudhari24@gmail.com>
2026-02-09 10:52:39 +0530

    Update main.jsx

--- CWS-Client/src/main.jsx
+++ CWS-Client/src/main.jsx
@@ -21,13 +21,10 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import { reactPlugin } from "./appInsights";
-import { AppInsightsContext } from "@microsoft/applicationinsights-react-js";
+import "./appInsights"; // 👈 side-effect import ONLY
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppInsightsContext.Provider value={reactPlugin}>
-      <App />
-    </AppInsightsContext.Provider>
+    <App />
   </StrictMode>
 );
