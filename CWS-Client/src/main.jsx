c5be172e1eeca4d1018fc047c95cbe8c47998405
meghlesh <meghachaudhari24@gmail.com>
2026-02-09 09:59:19 +0530

    Integrate Bootstrap and App Insights in main.jsx
    Added Bootstrap CSS and JS imports, and integrated App Insights context provider.

--- CWS-Client/src/main.jsx
+++ CWS-Client/src/main.jsx
@@ -1,3 +1,4 @@
+/**
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -11,3 +12,22 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+**/
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import { reactPlugin } from "./appInsights";
+import { AppInsightsContext } from "@microsoft/applicationinsights-react-js";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AppInsightsContext.Provider value={reactPlugin}>
+      <App />
+    </AppInsightsContext.Provider>
+  </StrictMode>
+);
